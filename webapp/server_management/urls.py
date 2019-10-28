4071fa1044de8ffdb50aa4c96b351be2680be305
Martin Mucha <martin.mucha.3@student.tuke.sk>
2019-10-28 19:55:29 +0100

    heroku commit

--- webapp/server_management/urls.py
+++ webapp/server_management/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from . import views
+from server_management.views import ServerDataView, ShowServerListToConnect, ServerInfoView
+
 
 app_name = 'server_management'
 urlpatterns = [
-    path('server_info', views.ServerInfoView.as_view(), name='info'),
-    path('server_data', views.ServerDataView.as_view(), name='data')
+    path('server_info/', ServerInfoView.as_view()),
+    path('server_data/', ServerDataView.as_view()),
+    path('server_call/', ShowServerListToConnect.as_view()),
+    path('server_call/<str:server_name>/', views.connect_to_server)
 ]
 
--- webapp/server_management/views.py
+++ webapp/server_management/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.http import Http404, HttpResponse
 from django.views import generic
 
 from .models import ServerInfo, ServerData
@@ -22,4 +23,21 @@ class ServerDataView(generic.ListView):
 
     def get_queryset(self):
         return ServerData.objects.all()
+
+
+class ShowServerListToConnect(generic.ListView):
+    template_name = 'server_management/server_list.html'
+    context_object_name = 'server_list'
+
+    def get_queryset(self):
+        return ServerInfo.objects.all()
+
+
+def connect_to_server(request, server_name):
+    try:
+        server = ServerInfo.objects.get(pk=server_name)
+    except ServerInfo.DoesNotExist:
+        raise Http404("Server does not exist")
+
+    return HttpResponse('Hey '+server_name+'<br>Your ip is:'+server.server_ip)
 
